22e1643957925600c16f695d769effe74ebf00a9
Jordan Yoon-Buck <jyoon-buck17@students.browndeerschools.com>
2017-05-10 08:59:42 -0500

    chose a better measurement method

--- src/distance.c
+++ src/distance.c
@@ -1,8 +1,8 @@
 #ifndef distance_
 #define distance_
 
-#define getDistance getDistance__perimeter
-// #define getDistance getDistance__linear
+// #define getDistance getDistance__perimeter
+#define getDistance getDistance__linear
 // #define getDistance getDistance__oneComponent
 
 #define _dargs int x1, int x2, int y1, int y2
